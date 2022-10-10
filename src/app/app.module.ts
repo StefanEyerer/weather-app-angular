563f33b3ed7f7ac55cb834ad13c9b752ab1f2b10
Stefan Eyerer <stefan.eyerer@gmail.com>
2022-10-10 10:32:00 +0200

    add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, MainComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
